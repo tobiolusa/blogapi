674e83645363796fe9323161d34101c9d5637d45
tobiolusa <olusamiracle@gmail.com>
2023-04-13 05:04:31 +0100

    added the urls and wrote the user list endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from api import serializers
+from rest_framework import generics
 
-# Create your views here.
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+    
+    
