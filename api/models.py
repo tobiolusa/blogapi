844d1fdb916ff8228ef849f8698df9586426e198
tobiolusa <olusamiracle@gmail.com>
2023-04-13 05:24:28 +0100

    Post model added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title= models.CharField(max_length=255, blank=True, default='')
+    content = models.TextField(blank=True, default='')
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('User', related_name="post", on_delete=models.CASCADE)
+    
+    class Meta:
+        def __str__(self):
+            return self.title
+        ordering = ['created_on']
+        
+    
